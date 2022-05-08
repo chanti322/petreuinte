4ae94ad809bd834c16ade7b60d26723359c76f87
lauratro <lauratronchin@hotmail.it>
2022-05-08 16:09:24 +0200

    Add margin to center content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+
 export default function Footer() {
-  let userAvatar = localStorage.getItem("userAvatar");
-  let userName = localStorage.getItem("usernameStorage");
+
   let history = useHistory();
   let footerStyle = {
     display: "flex",
@@ -16,6 +15,7 @@ export default function Footer() {
     position: "fixed",
     zIndex: 10,
     bottom: 0,
+    left:0
   };
   let footerText = {
     display: "flex",
@@ -44,21 +44,7 @@ export default function Footer() {
         <p style={footerText}> FindMyPet</p>
 
         <p style={footerText}> | Created by Laura Tronchin |</p>
-        {/* {userAvatar && (
-          <Link to="userProfile">
-            <img
-              src={userAvatar}
-              style={{
-                width: 40,
-                position: "absolute",
-                top: 10,
-                right: 10,
-                borderRadius: "100%",
-              }}
-              alt="avatar"
-            />
-          </Link>
-        )} */}
+     
       </div>
     </div>
   );
